6a7ade082bb825ab7576a076ade42b8cc96ffc3c
Yixin Zhao <st97334@gmail.com>
2024-01-30 12:53:02 -0500

    file upload

---
+++ unit1Proj/unit1Proj/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  unit1Proj
+//
+//  Created by 趙藝鑫 on 1/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTap(_ sender: UITapGestureRecognizer) {
+        if let tappedView = sender.view {
+            performSegue(withIdentifier: "detailSegue", sender: tappedView)
+        }
+    }
+    
+}
+
