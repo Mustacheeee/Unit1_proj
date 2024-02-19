4ece51f1998652931b3dc69b7812c32dc38dd700
Yixin Zhao <st97334@gmail.com>
2024-02-18 19:50:25 -0500

    Dynamic UI label done

--- unit1Proj/unit1Proj/DetailViewController.swift
+++ unit1Proj/unit1Proj/DetailViewController.swift
@@ -8,15 +8,19 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    var info: String?
+    @IBOutlet weak var describtions: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        describtions.text = info
         // Do any additional setup after loading the view.
     }
     
 
-    /*
+    	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -24,6 +28,6 @@ class DetailViewController: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
--- unit1Proj/unit1Proj/ViewController.swift
+++ unit1Proj/unit1Proj/ViewController.swift
@@ -8,15 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backstoryButton: UIButton!
+    
+    @IBOutlet weak var neighbothoodButton: UIButton!
+    
+    @IBOutlet weak var jobButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! DetailViewController
+        destVC.info = sender as? String
+    }
     @IBAction func didTap(_ sender: UITapGestureRecognizer) {
         if let tappedView = sender.view {
-            performSegue(withIdentifier: "detailSegue", sender: self)
+            if tappedView == backstoryButton {
+                let info = "SpongeBob is a childish, joyful, and clumsy sea sponge who lives in a pineapple with his pet snail Gary in the underwater city of Bikini Bottom."
+                performSegue(withIdentifier: "detailSegue", sender: info)
+                print("backstory button tapped!")
+            } else if tappedView == neighbothoodButton {
+                let info = "SpongeBob lives in a neighborhood with two neighbors: Squidward Tentacles and Patrick Star"
+                performSegue(withIdentifier: "detailSegue", sender: info)
+                print("neighbor hood button tapped!")
+            } else if tappedView == jobButton {
+                let info = "SpongeBob works as a fry cook at the Krusty Krab, a job in which he is exceptionally skilled at and enjoys thoroughly. He enjoys his job more than any other activity and is saddened whenever he cannot be at work."
+                performSegue(withIdentifier: "detailSegue", sender: info)
+                print("job button tapped!")
+            }
+            
         }
     }
     
